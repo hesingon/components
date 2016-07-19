4b17e1bb3d8afbd742f4c266ede8a785a6c26a62
Darius <e0007888@u.nus.edu>
2016-07-19 19:35:59 +0800

    First Commit

---
+++ components/signup/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	$('form').submit(function() {
+		var firstName = $('#first').val();
+		
+		if(firstName === "") {
+			$('.first-name-error').text('Please enter your first name.')
+		}else{$('.first-name-error').text('')}
+
+		var lastName = $('#last').val();
+		if(lastName === ''){
+			$('.last-name-error').text('Please enter your last name.')
+		}else{$('.last-name-error').text('')}
+
+		var email = $('#email').val();
+		if(email === ''){
+			$('.email-error').text('Please enter your email address.')
+		}
+		else if(email === 'hexing.happy@163.com'){
+			$('.email-error').text('This email is already taken.')
+		}else{$('.email-error').text('')}
+
+		var password = $('#password').val();
+		if(password === ''){
+			$('.password-error').text('Please enter a password.')
+		}
+		else if(password.length < 8){
+			$('.password-error').text('Short passwords are easy to guess. Try one with at least 8 characters')
+		}else{$('.password-error').text('')}
+
+		return false;
+	})
+});
